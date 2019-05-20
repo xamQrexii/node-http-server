164547f75311cb79068fb606ac6b5967a8900dcd
xamqrexii <usama1630@gmail.com>
2019-05-20 22:03:38 +0500

    Node HTTP Example 2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// import http core module
+// import core modules
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 // initialize variables for hostname and port
 const port = 3000;
@@ -7,10 +9,42 @@ const hostname = 'localhost';
 
 // create http server
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-});
+    console.log('Request for ' + req.url + ' by method ' + req.method);
+  
+    if (req.method == 'GET') {
+      var fileUrl;
+      if (req.url == '/') fileUrl = '/index.html';
+      else fileUrl = req.url;
+  
+      var filePath = path.resolve('./public'+fileUrl);
+      const fileExt = path.extname(filePath);
+      if (fileExt == '.html') {
+        fs.exists(filePath, (exists) => {
+          if (!exists) {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                        ' not found</h1></body></html>');
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          fs.createReadStream(filePath).pipe(res);
+        });
+      }
+      else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                ' not a HTML file</h1></body></html>');
+      }
+    }
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + req.method + 
+                ' not supported</h1></body></html>');
+    }
+  });
 
 server.listen(port, hostname, () => console.log(`Server is running on http://${hostname}:${port}`));
